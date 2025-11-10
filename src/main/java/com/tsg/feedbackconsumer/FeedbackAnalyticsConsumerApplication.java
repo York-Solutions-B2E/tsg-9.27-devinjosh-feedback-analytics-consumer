dba6ad4e52e431bbf5d16a6808e0abd7e05d439e
devin_mason <mvp@vastofi.com>
2025-11-10 16:13:32 -0500

    FEAT: Impelement spring boot skeleton

---
+++ src/main/java/com/tsg/feedbackconsumer/FeedbackAnalyticsConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.tsg.feedbackconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedbackAnalyticsConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeedbackAnalyticsConsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tsg/feedbackconsumer/FeedbackAnalyticsConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tsg.feedbackconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedbackAnalyticsConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
